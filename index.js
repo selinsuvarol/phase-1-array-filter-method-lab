7c11536f62c328461cd3982d285349ff653004a2
Selin Su Varol <selinsuvarol@gmail.com>
2022-04-20 13:55:26 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const { stringMatching } = require("expect");
+
+function findMatching(arr, string) {
+    return arr.filter((arritem) => 
+        arritem.toLowerCase() === string.toLowerCase());
+  };
+
+function fuzzyMatch(arr, string) {
+    return arr.filter(
+      (arritem) =>
+        arritem.toLowerCase().indexOf(string.toLowerCase()) === 0
+    );
+  };
+  
+function matchName(arrOfObj,string) {
+    return arrOfObj.filter((objElements)=> objElements.name === string)
+};
